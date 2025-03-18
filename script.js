273858e7dfcfbcb1c730a158ac2e66187e806ec7
Naufal <naufalbhurr@gmail.com>
2025-03-18 12:46:29 +0700

    update

--- script.js
+++ script.js
@@ -7,15 +7,18 @@ $(document).ready(function () {
       data.forEach((member) => {
         let marqueeItem = `
           <div class="marquee-item card mx-2 position-relative">
-          <div class="card-background"></div>          
-          <img src="${member.foto}" class="card-img-top member-foto" alt="${member.name}">
-          <div class="card-body position-relative z-index-1 bg-white h-100">
-            <h5 class="card-title">${member.name}</h5>
-            <p class="card-subtitle mb-2 text-muted">${member.role}</p>
-            <p class="card-text"><small>"${member.quote}"</small></p>
-            <div class="text-muted"><small>@${member.username}</small></div>
+            <div class="card-background"></div>
+            <img src="${member.foto}" class="card-img-top member-foto" alt="${member.name}">
+            <div class="card-body position-relative z-index-1 bg-white">
+              <div class="card-container">
+                <h5 class="card-title">${member.name}</h5>
+                <p class="card-subtitle mb-2 text-muted">${member.role}</p>
+                <p class="card-text"><small>"${member.quote}"</small></p>
+                <div class="text-muted"><small>@${member.username}</small></div>
+              </div>
+              <a href="${member.nickname}.html" class="btn btn-primary position-absolute bottom-0 start-0">See Portfolio</a>
+            </div>
           </div>
-        </div>
         `
         marqueeInner.append(marqueeItem)
       })
@@ -28,6 +31,7 @@ $(document).ready(function () {
         direction: "left",
         duplicated: true,
         startVisible: true,
+        pauseOnHover: true,
       })
     }).fail(function () {
       console.error("Error loading JSON data.")
